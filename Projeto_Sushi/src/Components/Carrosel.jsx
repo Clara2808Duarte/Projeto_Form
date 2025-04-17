6a9107ed59e20c82d9f4b8a25607128b9daeb827
Ju Piazzoli <jpdomeneghetti@gmail.com>
2025-04-17 13:30:38 -0300

    Carrosel - Atualização

--- Projeto_Sushi/src/Components/Carrosel.jsx
+++ Projeto_Sushi/src/Components/Carrosel.jsx
@@ -5,43 +5,59 @@ import Img4 from "../assets/Img4.jpg";
 import "./Carrosel.css";
 
 function Carrosel() {
+  // Declara um estado para o índice do slide atual, inicializado em 0
   const [currentIndex, setCurrentIndex] = useState(0);
-  const slides = [Img2, Img3, Img4]; // Array de imagens
+  
+  // Array de imagens que serão exibidas no carrossel
+  const slides = [Img2, Img3, Img4]; 
+  // Armazena o total de slides disponíveis
   const totalSlides = slides.length;
 
-  // Função para mover os slides
+  // Função para mover os slides, recebe um parâmetro 'step' que indica a direção
   const moveSlide = (step) => {
+    // Calcula o novo índice com base no índice atual e no passo
     let newIndex = currentIndex + step;
+    
+    // Se o novo índice for maior ou igual ao total de slides, volta para o primeiro slide
     if (newIndex >= totalSlides) {
       newIndex = 0;
-    } else if (newIndex < 0) {
+    } 
+    // Se o novo índice for menor que 0, vai para o último slide
+    else if (newIndex < 0) {
       newIndex = totalSlides - 1;
     }
+    
+    // Atualiza o estado com o novo índice
     setCurrentIndex(newIndex);
   };
 
-  // Atualiza o slide automaticamente a cada 5 segundos
+  // Efeito colateral para atualizar o slide automaticamente a cada 5 segundos
   useEffect(() => {
+    // Define um intervalo que chama a função moveSlide a cada 5000 milissegundos (5 segundos)
     const intervalId = setInterval(() => {
       moveSlide(1);
     }, 5000);
 
-    return () => clearInterval(intervalId); // Limpa o intervalo ao desmontar o componente
-  }, [currentIndex]); // Dependência no currentIndex para reiniciar o intervalo
+    // Retorna uma função de limpeza que limpa o intervalo quando o componente é desmontado
+    return () => clearInterval(intervalId); 
+  }, [currentIndex]); // Dependência no currentIndex para reiniciar o intervalo quando o índice mudar
 
   return (
     <section className="carousel">
+      {/* Seção que contém os slides, com uma transformação CSS para mover os slides */}
       <section className="slides" style={{ transform: `translateX(-${100 * currentIndex}%) `}}>
+        {/* Mapeia cada slide para um elemento de imagem */}
         {slides.map((slide, index) => (
           <img key={index} src={slide} alt={`Imagem ${index + 1}`} />
         ))}
       </section>
       
+      {/* Botão para mover para o slide anterior */}
       <button className="prev" onClick={() => moveSlide(-1)}>&#10094;</button>
+      {/* Botão para mover para o próximo slide */}
       <button className="next" onClick={() => moveSlide(1)}>&#10095;</button>
-     
     </section>
   );
 }
 
-export default Carrosel;
+export default Carrosel; // Exporta o componente Carrosel para uso em outros arquivos
