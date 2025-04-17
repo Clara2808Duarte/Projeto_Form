2fdfa64188aff2071fce0ccf315d7004181f40a6
Ju Piazzoli <jpdomeneghetti@gmail.com>
2025-04-17 13:30:49 -0300

    Footer - Modificação

--- Projeto_Sushi/src/Components/Footer.jsx
+++ Projeto_Sushi/src/Components/Footer.jsx
@@ -1,12 +1,18 @@
-import './Footer.css'
+import './Footer.css';
+import Collab from '../assets/Colab.png'
 
 function Footer() {
   return (
-    <footer className="footer">
+    <>
+    <footer className='Flex-Row'>
+      <section  className="footer">
       <p>Urban Sushi - Rua das Sushi Lovers, 123 - São Paulo, SP</p>
       <p>Telefone: (11) 1234-5678 | Email: contato@urbansushi.com.br</p>
       <p>&copy; 2025 Urban Sushi. Todos os direitos reservados.</p>
+      </section>
     </footer>
+    <img className="ImgCollab" src={Collab} alt="Collab" />
+    </>
   );
 }
 
